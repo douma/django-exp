f20326845d5824190aab27497f792577f4e80674
Stefan Douma <douma4@gmail.com>
2019-05-23 21:55:36 +0200

    Added home template and readme

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Tutorial
+
+https://www.youtube.com/watch?v=F5mRW0jo-U4
--- src/pages/views.py
+++ src/pages/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello world</h1>");
+    return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello contact</h1>");
