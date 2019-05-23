b3af096c55b4d9326dbe74c32562c8e133afe898
Stefan Douma <douma4@gmail.com>
2019-05-23 22:14:07 +0200

    Played with template

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request, "home.html", {})
+    my_context = {
+        "my_text": "Hello text",
+        "my_number": 1234,
+        "my_list": [
+            123, 456, 789
+        ]
+    }
+    return render(request, "home.html", my_context)
 
 def contact_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello contact</h1>");
